6344d409c56ae5ba321a3d874d7e936eb4b84823
JakubVavru <60426585+JakubVavru@users.noreply.github.com>
2020-05-26 09:17:25 +0200

    zpřeházet celý kod

--- index.php
+++ index.php
@@ -15,38 +15,44 @@ var_dump($submit);
 </head>
 <body>
 
+
+
+<?php
+
+    if(isset($submit)) {
+?> 
+
+    <h2>Formulář byl odeslán</h2>
+    Email: <?= $email ?></br>
+    Pohlaví: <?= $gender ?>
+
+<?php
+    } else { 
+?>
 <form action="index.php" method="post">
-<label for="email">E-mail</label>
-<input type="email" name="email" id="email">
+    <label for="email">E-mail</label>
+    <input type="email" name="email" id="email">
 
-<h2>Pohlaví</h2>
-<input type="radio" name="gender" value="male" id="gender-male">
-<label for="gender-male">Muž</label>
-</br>
+    <h2>Pohlaví</h2>
+    <input type="radio" name="gender" value="male" id="gender-male">
+    <label for="gender-male">Muž</label>
+    </br>
 
-<input type="radio" name="gender" value="female" id="gender-female">
-<label for="gender-female">Žena</label>
-</br>
+    <input type="radio" name="gender" value="female" id="gender-female">
+    <label for="gender-female">Žena</label>
+    </br>
 
-<input type="radio" name="gender" value="other" id="gender-other">
-<label for="gender-other">Nespecifikováno</label>
-</br>
+    <input type="radio" name="gender" value="other" id="gender-other">
+    <label for="gender-other">Nespecifikováno</label>
+    </br>
 
-<input type="submit" name="submit" value="Odeslat">
+    <input type="submit" name="submit" value="Odeslat">
 </form>
 
-<?php
-
-if(isset($submit)) {
-?> <h2>Formulář byl odeslán</h2>
-Email: <?= $email ?></br>
-Pohlaví: <?= $gender ?>
-<?php
-} else {?>
    <h2>Stránka byla načtena bez formuláře</h2>
-   <?php
+   
+<?php
 }
-
 ?>
 
 </body>
